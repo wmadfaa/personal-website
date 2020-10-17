ff423480be02af67a2b10e9e1a2292a16845505f
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-10-17 15:22:12 +0200

    add typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
             },
         },
         `gatsby-plugin-react-helmet`,
+        'gatsby-plugin-typescript',
         {
             resolve: `gatsby-source-filesystem`,
             options: {
